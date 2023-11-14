5750a0f1df21eb494f0fff7e9509cb631e3edb81
PichlerMathias <s2210307788@fhooe.at>
2023-11-14 20:15:26 +0100

    removed log

--- pages/timer/clock.js
+++ pages/timer/clock.js
@@ -31,7 +31,6 @@ export function getRemainingSeconds(callback) {
 }
 
 export function initClock(callback) {
-    console.log("init clock called");
     initClockHtml();
     getRemainingSeconds(function (remainingSeconds, limit) {
         if (remainingSeconds != null && remainingSeconds > 0) {
